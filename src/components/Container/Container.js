d968f347e38212b7f9b6346700fbdafd8bd3ba8e
Flávio Marques <flaviomarques2@hotmail.com>
2023-05-25 17:37:17 -0300

    criei componente de Container principal que será usado para todas as páginas

---
+++ src/components/Container/Container.js
@@ -0,0 +1,12 @@
+import './Container.css';
+import MenuLateral from '../MenuLateral/MenuLateral';
+
+const Container = () => {
+    return (
+        <div className='container'>
+            <MenuLateral/>
+        </div>
+    )
+}
+
+export default Container
---
+++ src/components/MenuLateral/MenuLateral.js
@@ -0,0 +1,17 @@
+import './MenuLateral'
+
+const MenuLateral = () => {
+    return(
+        <div className='container_lateral'>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+                
+
+        </div>
+    )
+}
+
+export default MenuLateral
